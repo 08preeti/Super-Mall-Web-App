d9496fc99e13a5950d1a59ceacfc1dd82ba5f929
08preeti <chikkalipreeti08@gmail.com>
2026-01-31 23:43:30 +0530

    Connected Firebase configuration

---
+++ js/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxbH8w8ShHNKXPk_O6dIMBGvXxHSlRgD0",
+  authDomain: "super-mall-web-app-2966c.firebaseapp.com",
+  projectId: "super-mall-web-app-2966c",
+  storageBucket: "super-mall-web-app-2966c.firebasestorage.app",
+  messagingSenderId: "765757747617",
+  appId: "1:765757747617:web:2107cb453679bbfe05dbe0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
